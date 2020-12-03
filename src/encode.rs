916e7a2aec0063934c8f175180728f53a800bf26
clearloop <udtrokia@gmail.com>
2020-12-04 03:38:21 +0800

    feat(encode): complete encode

--- src/encode.rs
+++ src/encode.rs
@@ -9,7 +9,7 @@ pub trait ToHex {
     fn hex(&self) -> Option<Self::Hex>;
 }
 
-impl<'x> ToHex for &'x u8 {
+impl<'x> ToHex for &u8 {
     type Hex = Hex;
     fn hex(&self) -> Option<Self::Hex> {
         Some([HEX[(*self >> 4) as usize], HEX[(*self & 0xf) as usize]])
@@ -17,17 +17,17 @@ impl<'x> ToHex for &'x u8 {
 }
 
 impl<'x> ToHex for &'x char {
-    type Hex = [Option<Hex>; 4];
+    type Hex = SliceVec<'x, char>;
     fn hex(&self) -> Option<Self::Hex> {
         let src = char2u8(&self)?;
         let mut dest = Self::Hex::default();
 
-        for mut i in 0..4 {
-            i = 3 - i;
-            if i == 0 {
+        for (p, q) in src.iter().enumerate() {
+            if 3 - p == 0 {
                 break;
             }
-            dest[i] = (&src[i]).hex();
+
+            dest.extend_from_slice(&SliceVec::from(&mut (&q).hex()?));
         }
 
         Some(dest)
@@ -41,6 +41,19 @@ impl<'x> ToHex for &'x [u8] {
         let mut dest: Self::Hex = SliceVec::default();
         for i in self.iter() {
             dest.extend_from_slice(&SliceVec::from(&mut i.hex()?));
+        }
+
+        Some(dest)
+    }
+}
+
+impl<'x> ToHex for &'x [char] {
+    type Hex = SliceVec<'x, char>;
+
+    fn hex(&self) -> Option<Self::Hex> {
+        let mut dest: Self::Hex = SliceVec::default();
+        for i in self.iter() {
+            dest.extend_from_slice(&[*i]);
         }
 
         Some(dest)
--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,11 @@ mod encode;
 mod util;
 mod vec;
 
+use self::{encode::ToHex, vec::SliceVec};
+
 /// Hex chars
 #[rustfmt::skip]
-pub const HEX: [char; 16] = [
+const HEX: [char; 16] = [
     '0', '1', '2', '3',
     '4', '5', '6', '7',
     '8', '9', 'a', 'b',
@@ -17,4 +19,13 @@ pub const HEX: [char; 16] = [
 ];
 
 /// Hex Primitive
-pub type Hex = [char; 2];
+type Hex = [char; 2];
+
+/// Encode str or bytes into hex iterator
+pub fn encode<'x, T>(target: &T) -> Option<impl From<SliceVec<'x, char>>>
+where
+    T: ToHex,
+    <T as encode::ToHex>::Hex: core::convert::From<SliceVec<'x, char>>,
+{
+    target.hex()?.into()
+}
